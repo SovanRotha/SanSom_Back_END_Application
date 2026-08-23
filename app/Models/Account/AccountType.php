80022ee4c8b5a869324c3e5e61ce55b62d601c5b
thida rek <thidaarek@gmail.com>
2026-08-23 16:35:38 +0700

    Add account and account type

---
+++ app/Models/Account/AccountType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class AccountType extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'icon',
+    ];
+
+    /**
+     * An account type can have many accounts.
+     */
+    public function accounts(): HasMany
+    {
+        return $this->hasMany(Account::class);
+    }
+}
